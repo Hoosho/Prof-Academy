42e201df797e4dc1df260ea75f232bc2cf2da7be
Hoosho <hisham4kn4mohamed@gmail.com>
2026-01-18 11:23:50 +0200

    Finish CRUD On Months Routes.

---
+++ src/routes/teacher/month.route.js
@@ -0,0 +1,66 @@
+// /src/routes/teacher/student.route.js
+import express from "express";
+const router = express.Router();
+
+// Import Middlewares
+import { authTeacher } from '../../middlewares/auth.middleware.js'
+import { contextMiddleware } from '../../middlewares/context.middleware.js';
+
+// Import Controllers
+import {
+  createMonth, getAllMonth, updateMonth, deleteMonth
+} from '../../controllers/teacher/month.controller.js'
+
+// Import Validations
+// import {
+// } from '../../validations/teacher/month.validation.js';
+/**
+ * @desc Create Month
+ * @route POST /api/teacher/month 
+*/
+router.post(
+  '/api/teacher/month',
+  authTeacher,
+  contextMiddleware,
+  ,
+  createMonth
+);
+
+/**
+ * @desc Get All Months
+ * @route GET /api/teacher/months
+*/
+router.get(
+  '/api/teacher/months',
+  authTeacher,
+  contextMiddleware,
+  getStudentsQueryValidation,
+  getAllMonth
+);
+
+/**
+ * @desc Update Month
+ * @route PUT /api/teacher/month/:id
+*/
+router.put(
+  '/api/teacher/month/:id',
+  authTeacher,
+  contextMiddleware,
+  ,
+  ,
+  updateMonth
+);
+
+/**
+ * @desc Delete Month
+ * @route Delete /api/teacher/month/:id
+*/
+router.delete(
+  '/api/teacher/month/:id',
+  authTeacher,
+  contextMiddleware,
+  ,
+  deleteMonth
+);
+
+export default router;
