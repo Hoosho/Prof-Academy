be316ba314cbe5cea8087ba23ab8cf6779ca2119
Hoosho <hisham4kn4mohamed@gmail.com>
2026-01-27 20:00:05 +0200

    Add Exam Model.

--- src/models/Exam.model.js
+++ src/models/Exam.model.js
@@ -1,7 +1,138 @@
 // models/Exam.model.js
 import mongoose from "mongoose";
 
-const ExamSchema = new mongoose.Schema();
+// Question Model
+const QuestionsSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: {
+      values: [ 'msq' ],
+      message: ' نوع السؤال غير صحيح! ' 
+    },
+    required: [ true, '❌ نوع الاختبار مطلوب' ],
+    default: 'msq'
+  },
+  text: {
+    type: String,
+    required: [ true, '❌ نص السؤال مطلوب!' ],
+    minlength: [ 5, '❌ نص السؤال قصير جدا!'],
+    maxlength: [ 250, '❌ نص السؤال كبير جدا!'],
+    trim: true
+  },
+  options: {
+    type: [String],
+    validate: {
+      validator: function (val) {
+        if (this.type === "mcq") {
+          return (
+            Array.isArray(val) &&
+            val.length >= 2 &&
+            val.length <= 4 &&
+            val.every((opt) => typeof opt === "string" && opt.length <= 150)
+          );
+        }
+        return true;
+      },
+      message:
+        "❌ اختيارات MCQ لازم تكون من 2-4 اختيارات، وكل اختيار ≤ 150 حرف",
+    },
+  },
+
+  correctIndex: {
+    type: Number,
+    required: [ ture, '❌ رقم الإجابة الصحيحة مطلوب!'],
+    validate: {
+      validator: function( val ){
+        return Number.isInteger( val ) && val >= 9;
+      },
+      message: '❌ رقم الإجابة الصحيحة غير صالح!'
+    }, 
+  }
+}, {
+  _id: true
+});
+
+// Exmas Model 
+const ExamSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [ true, '❌ عنوان الاختبار مطلوب!' ],
+    trim: true,
+    minlength: [ 3, '❌ عنوان الاختبار قصير جدا!' ],
+    maxlength: [ 120, '❌ عنوان الاختبار طويل جدا!' ]
+  },
+  type: {
+    type: String,
+    enum: [ 'msq', 'truefalse', 'short' ],
+    default: 'msq'
+  },
+  grade: {
+    type: String,
+    enum: {
+      values: [
+      "الصف الاول الاعدادي",
+      "الصف الثاني الاعدادي",
+      "الصف الثالث الاعدادي",
+      "الصف الاول الثانوي",
+      "الصف الثاني الثانوي",
+      "الصف الثالث الثانوي",
+    ],
+    message: '❌ الصف الدراسي غير صالح!'
+    },
+    required: [true, "الصف الدراسي مطلوب!"],
+  },
+  status: {
+    type: String,
+    enum: {
+      values: [ 'active', 'hidden', 'inactive' ],
+      message: '❌ حالة الصف غير صالحة!'
+    },
+    default: 'inactive'
+  },
+  durationMinutes: {
+    type: Number,
+    required: [ true, '❌ مدة الإختبار مطلوبة!' ],
+    min: [ 1, '❌ مدة الإختبار قصيرة جدا!' ],
+    max: [ 180, '❌ مدة الإختبار طويلة جدا!' ],
+  },
+  totalMarks: {
+    type: Number,
+    required: [ true, '❌ مجموع دراجات الإختبار مطلوبة'],
+    min: [ 1, '❌ مجموع دراجات الإختبار صغيرة جدا!'],
+    max: [ 120, '❌ مجموع دراجات الإختبار كبيرة جدا!'],
+  },
+
+  questions: {
+    type: [ QuestionsSchema ],
+    validate: [
+      {
+        validateor: ( v ) => Array.isArray( v ) && v.length > 0 && v.length <= 120,
+        message: "❌ لازم يكون في 1-120 سؤال بالاختبار",
+      }
+    ]
+  },
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Teacher',
+    required: [ true, 'معرف المعلم مطلوب!' ]
+  },
+
+  // Soft Delete
+  isDeleted: {
+    type: Boolean,
+    default: false
+  },
+  deletedAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timeStamps: true
+});
+
+// Indexes
+ExamSchema.index({ teacher: 1, status: 1, grade: 1, isDeleted: 1 });
+ExamSchema.index({ title: 'text' });
 
 // Export Exam Model
 export default mongoose.model( 'Exam', ExamSchema );
